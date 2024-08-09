ceea2c0d188bdaebc3fb66c1c7ccae151511d920
Lai Chang <laichangdiaz@gmail.com>
2024-08-09 19:30:48 -0400

    age calulator

--- builderNotesApp/builderNotes.js
+++ builderNotesApp/builderNotes.js
@@ -2,6 +2,15 @@ const notesContaienr = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".notes-add");
 const notes = document.querySelector(".input-box");
 
+function showNotes(){
+    notesContaienr.innerHTML =localStorage.getItem("notes");
+}
+showNotes();
+
+//guarda info en el navegador
+function updateStorage(){
+    localStorage.setItem("notes", notesContaienr.innerHTML);
+}
 createBtn.addEventListener("click", () =>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -15,5 +24,13 @@ createBtn.addEventListener("click", () =>{
 notesContaienr.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
+        updateStorage()
+    }else if(e.target.tagName === "P"){
+        notes =document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
     }
 })
